0cce59e8457a311ab55958367bb649438c314235
Marine <marine.richer41@gmail.com>
2024-08-21 12:08:59 +0200

    config

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -12,20 +12,23 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+env = environ.Env()
+environ.Env.read_env(env_file=str(BASE_DIR / "blog" / ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "ccq1&k=+x%83f(1ml@04fvp_vf)zs7by)%kp0#4)+iwt9az5=$"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
